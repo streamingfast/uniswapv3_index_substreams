976773d4866bb1a79f6bdb864503ad102ee98dd6
arnaudberger <arnaudberger84@gmail.com>
2024-05-05 09:40:00 +0200

    First commit

---
+++ src/pb/mod.rs
@@ -0,0 +1,20 @@
+// @generated
+pub mod contract {
+    // @@protoc_insertion_point(attribute:contract.v1)
+    pub mod v1 {
+        include!("contract.v1.rs");
+        // @@protoc_insertion_point(contract.v1)
+    }
+}
+
+pub mod sf {
+    pub mod substreams {
+        pub mod index {
+            // @@protoc_insertion_point(attribute:sf.substreams.index.v1)
+            pub mod v1 {
+                include!("sf.substreams.index.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.index.v1)
+            }
+        }
+    }
+}
